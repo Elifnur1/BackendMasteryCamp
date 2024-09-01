cce045340515c64d7be42b7872c715b8e229be3d
Elifnur1 <elifnurbinici@hotmail.com>
2024-09-01 16:01:45 +0300

    18-08-2024

---
+++ Homework18_08_2024/Program.cs
@@ -0,0 +1,175 @@
+﻿using System.Security.Cryptography;
+
+namespace Homework18_08_2024;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /* 1-
+        int a=12;
+        int b=8;
+        Console.WriteLine(a+b);*/
+
+
+        /* 2-
+        double x=10.5d;
+        double x_karesi=x * x;
+        double x2= x_karesi;
+        Console.WriteLine(x2);*/
+
+        /*    3-
+        string Myname="Elifnur";
+        int Myage=23;
+         string Myage2= Myage.ToString();
+         Console.WriteLine(Myage2);
+        string a= "Benim adım" + " " + Myname + " "+ "ve" + " " + "ben" + " " +Myage2 + " " + "yaşındayım.";
+         Console.WriteLine(a);
+        */
+
+        /*  4-
+        char b='A';
+        int b1=65;
+        int b2=Convert.ToInt16(b1);
+        Console.WriteLine(b1)*/
+
+        /*   5-
+        bool varMi=true;
+        bool gecerliMi=false;
+        Console.WriteLine(varMi);*/
+
+        /*   6-
+        byte Z=255;
+        int Z2=Z;
+        int Z3=Z2;
+        Console.WriteLine(Z3); */
+
+        /*   7-
+        int Number=1000000;
+        byte Numberbyte= Convert.ToByte(Number);
+        byte Numberbyte2=Numberbyte;
+        Console.WriteLine(Numberbyte2); 
+        Anlık olarak dönüştürme yapıyor fakat çok büyük bir değeri çok küçük değere çevirdiği için ileride bu kod sana sorun veya hata çıkarabilir diyor.*/
+
+        /*   8-
+        double pi=3.14;
+        int piInt=Convert.ToInt32(pi);
+        int piInt2=piInt;
+        Console.WriteLine(piInt2);*/
+
+
+        /*   9-
+        string Value="42";
+        int Value2=Convert.ToInt32(Value);
+        Console.WriteLine(Value2); */
+
+        /*   10-
+        int number1=5;
+        int number2=10;
+        int number3=15;
+        int average=(number1+number2+number3)/3;
+        double average2=average;
+        Console.WriteLine(average2); */
+
+        /*  11-
+        decimal money=10.25m;
+        decimal money2=5.75m;
+        decimal toplam_deger= money+money2;
+        decimal money_toplam_deger=toplam_deger;
+        Console.WriteLine(money_toplam_deger); */
+
+
+        /*  12-
+       float number=1.23f;
+       double number2=number;
+       double number3=number2;
+       Console.WriteLine(number3);*/
+
+
+        /*  13-
+        long rate=5000000000;
+        int newrate=Convert.ToInt32(rate);
+        Console.WriteLine(newrate);  --Soru 7 deki gibi aynı uyarıyı verdi.*/
+
+        /*  14-
+        string name="Elifnur";
+        string surname="Binici";
+        string name_surname= name + " " + surname;
+        Console.WriteLine(name_surname); */
+
+        /*  15-  HOCAYA SORR!
+        string gecerliMi="true";
+        bool result=bool.Parse(gecerliMi);
+        bool newresult=result;
+        Console.WriteLine(newresult);*/
+
+        /*  16-
+       double pi=3.14159007654;
+       float newpi=Convert.ToSingle(pi);
+       float newpi2=newpi;
+       Console.WriteLine(newpi2); */
+
+        /*  17-
+        byte number=42;
+        short number2=42;
+        int number3=42;
+        long number4=42;
+        Console.WriteLine(sizeof(byte));
+        Console.WriteLine(sizeof(short));
+        Console.WriteLine(sizeof(int));
+        Console.WriteLine(sizeof(long)); */
+
+        /*  18-
+        string love="Aleynayı çok seviyorum";
+        int newlove=love.Length;
+        Console.WriteLine("Dizgedeki karakter sayısı:" +newlove); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
