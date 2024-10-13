af23604630fb9d3b169d59fd38b6faa36cae79d7
Elifnur1 <elifnurbinici@hotmail.com>
2024-10-13 12:09:18 +0300

    jQuary-week10

---
+++ week10/04-JavaScript/JavaScript kopyası/script.js
@@ -0,0 +1,6 @@
+document.getElementById('button-external').addEventListener('mouseover',function(){
+    alert('İşte üzerime geldin');
+})
+document.getElementById('button-external').addEventListener('doubleclick',function(){
+    alert('İşte çift tıkladın')
+})
