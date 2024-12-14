54f474ef08e6aa3f0ecd3af65ed0e02710321c8f
Elifnur1 <elifnurbinici@hotmail.com>
2024-12-14 11:48:22 +0300

    LINQ VE EF CORE

---
+++ ileri seviye C#/14-12-2024/LINQ VE EF CORE/Proje10_LINQ_Samples /Program.cs
@@ -0,0 +1,114 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using LINQ_Samples;
+using Proje10_LINQ_Samples;
+
+Repository repository = new Repository();
+
+#region Tüm Kategorileri Listeleme 
+// var result=repository.Categories.ToList();
+// var result=from Category in repository.Categories
+//  select Category;   
+// Console.ReadLine(); 
+#endregion
+#region Tüm Kategorielrin Adını Listeleme
+
+// var categoryNames=repository
+// .Categories
+// .Select(x=>x.Name)
+//     // .ToList();
+// var categoryNames=from Category in repository.Categories
+// select Category.Name;
+#endregion
+#region Sadece Silinmemiş Kategorileri Listeleme 
+// var deletedCategories=repository
+// .Categories
+// .Where(x=>!x.IsDeleted)
+// .ToList();
+#endregion
+#region Ürünlerin fiyatlarını Listeleme
+//    var Prices=repository
+//    .Products
+//    .Select(x=>x.Price)
+//    .ToList();
+
+#endregion
+#region Kategoriye Göre Gruplanmış Ürünleri Listeleme
+// var groupByCategory=repository
+// .Products
+// .GroupBy(x=>x.CategoryId)
+// .ToList();
+// var groupByCategory=from product in repository.Products
+// group product by product.CategoryId;
+
+#endregion
+#region Tedarikçi Başına Ürün Sayısını Listeleme
+// var productCountBySupllier=repository
+// .Products
+// .GroupBy(p=>p.SupplierId)
+// .Select(g=> new {SupplierId=g.Key,ProductCount=g.Count()})
+// .ToList();
+// var productCountBySupllier=
+// from product in repository.Products
+// group product by product.SupplierId into groupedData
+// select new {SupplierId=groupedData.Key,ProductCount=groupedData
+// .Count()};
+#endregion
+#region Her Kategorideki ilk ürünü getirme
+// var firstProductInCategory=
+// repository
+// .Products
+// .GroupBy(p=>p.CategoryId)
+// .Select(g=>g.FirstOrDefault())
+// .ToList();
+// var firstProductInCategory=
+// from product in repository.Products
+// group product by product.CategoryId into productGroup
+// select productGroup.FirstOrDefault();
+#endregion
+#region Ürün fiyatlarından en küçüğünü getirme
+// var minPrice = repository
+// .Products
+// .Min(p => p.Price);
+//tek bir ürün getirceği için tolist methodunu kullanmamıza gerek yok
+// var minPrice=(from product in repository.Products
+// select product.Price).Min();
+#endregion
+#region ÖRNEK LINQ SORGULARIM
+//1-
+var FullCategory = repository
+.Categories
+.ToList();
+var FullCategory2 = from Category in repository.Categories select Category;
+//2-Fiyatları belli aralıkta olan ürünleri getirme.
+var result = repository
+.Products
+.Where(x => x.Price >= 100 && x.Price <= 500 && !x.IsDeleted)
+.ToList();
+//3-Ürünleri fiyatına göre sıralama Artan.
+//Query Syntax
+var result2 = from Product in repository.Products orderby Product.Price select Product.Price;
+
+
+#endregion
+#region Bir müşteriye ait satışları getirme
+// var customerSales=
+// repository
+// .Sales
+// .Where(x=>x.CustomerId==3)
+// // .ToList();
+// var customerSales= from Sale in repository.Sales
+// where Sale.CustomerId==3
+// select Sale; 
+#endregion
+#region kategoriye göre ürünlerin fiyat ortalamalarını getirme
+// var averagePriceByCateegory =
+// repository
+// .Products
+// .GroupBy(p => p.CategoryId)
+// .Select(g => new { CategoryId = g.Key, AveragePrice = g.Average(p => p.Price) })
+// // .ToList();
+// var averagePriceByCateegory=from Product in repository.Products
+// group Product by Product.CategoryId into productGroup
+// select new{CategoryId=productGroup.Key, averagePrice=productGroup.Average(p=>p.Price)};
+
+#endregion
---
+++ ileri seviye C#/14-12-2024/LINQ VE EF CORE/Proje11_Collections/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Collections:C# içerisinde dizi benzeri bir veri yapısı ifade eder.Bir kaç türü vardır.
+
+1)IEnemureble: En temel collections yapılarından biridir.Ve şu özelliklere sahiptir;
+               *Iteration sağlar(döngü kullanabilme)
+               *Verilerin sadece OKUNMASI GEREKEN durumlarda tercih edilir.
+               *Bu durumda ekleme silme gibi operasyonalra izin vermez.
+               *Özellikle for each döngüsü ile kullanmaya oldukça uygundur.
+               *Verileri bellleğe almadan işlem yapar.
+*/
+// List<string>names=new List<string>{"Ali","Veli","Sezen","Murat"};
+// IEnumerable<string>enumarableNames=names;
+// foreach (var name in enumarableNames)
+// {
+//     System.Console.WriteLine(name);
+// }
+
+/*
+2)ICollection:IEnumarable'dan türetilmiştir.Ek olarak şu özelliklere sahiptir:
+              *Veri ekleme ve silme operasyonlarını destekler.
+              *Eleman sayısını öğrenme imkanı sağlar,
+              *Veri üzerinde iteration yaparken manipülasyon yapma ihtiyacı olan duurmlarda tercih edilir.
+*/
+// ICollection<string>names=new List<string>{"Ali","Veli","Sezen","Murat"};
+// System.Console.WriteLine(names.Count);
+
+/*
+3)IList:ICollection'dan türetilmiştir.Ek olarak sahip olduğu özellikler:,
+        *Indeksleme imkanı sunar.Bu sayede collection elemanlarına bir index numarası ile erişilebilir.([])
+        *Sıralı veri yapılarında tercih edilebilir.
+*/
+// IList<string>names=new List<string>{"Ali","Veli","Sezen","Murat"};
+// System.Console.WriteLine(names[0]);
+
+
+/*
+4)IQuerable:Bu collection yapısı biraz daha özel amaçlara hizmet eder:
+           *Daha çok veri tabanı sorguları oluşturmak için tercih edilir.
+           *Sorguyu,veri tabanına göndermeden önce optimize 
+           *Büyük veri setlerinde çok ciddi bir peformans sağlar.
+           *Filtreleme operasyonlarını direkt olarak veri tabanı üzerinde yaparak,hız kazandırır.
+*/
+//IEnumerable verileri belleğe çeker filtreleri orada yapar.Bellek daha hızlı yaptığı için oldukça performansı yüksek bir işlemdir.Her veriyi belleğe çekme işlemi yapmamayı tercih ederiz çünkü yer kaplamasını her seferinde istemeyiz.
