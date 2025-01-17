082359220854345bc71cda0f65314bdce057a920
Elifnur1 <elifnurbinici@hotmail.com>
2025-01-17 15:35:43 +0300

    AuthManager_Register_Metotu

--- ileri seviye C#/week4/29-12-2-2024/EShop_Repeat/EShop.Service/Concrete/AuthManager.cs
+++ ileri seviye C#/week4/29-12-2-2024/EShop_Repeat/EShop.Service/Concrete/AuthManager.cs
@@ -65,14 +65,55 @@ public class AuthManager : IAuthService
         }
     }
 
-    private async Task GenerateJwtToken(ApplicationUser user)
-    {
-        throw new NotImplementedException();
-    }
 
-    public Task<ResponseDto<ApplicationUserDto>> RegisterAsync(RegisterDto registerDto)
+    public async Task<ResponseDto<ApplicationUserDto>> RegisterAsync(RegisterDto registerDto)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var existingUser = await _useManager.FindByNameAsync(registerDto.UserName);
+            if (existingUser != null)
+            {
+                return ResponseDto<ApplicationUserDto>.Fail("bu kullanıcı adı zaten kullanılmakta", StatusCodes.Status400BadRequest);
+            }
+            var user = new ApplicationUser(
+                firstName: registerDto.FirstName,
+                lastName: registerDto.LastName,
+                dateOfBirth: registerDto.DateOfBirth,
+                gender: registerDto.Gender
+
+            )
+            {
+                UserName = registerDto.UserName,
+                Email = registerDto.Email,
+                EmailConfirmed = true,
+                Address = registerDto.Address,
+                City = registerDto.City
+            };
+            var result = await _useManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded)
+            {
+                return ResponseDto<ApplicationUserDto>.Fail("Kullanıcı oluşturulurken bir hata oluştu", StatusCodes.Status400BadRequest);
+            }
+            result = await _useManager.AddToRoleAsync(user, registerDto.Role);
+            if (!result.Succeeded)
+            {
+                return ResponseDto<ApplicationUserDto>.Fail("kullanıcı rolü atanırken bir hata oluştu", StatusCodes.Status400BadRequest);
+            }
+            var userDto = new ApplicationUserDto
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Address = user.Address,
+                City = user.City
+            };
+            return ResponseDto<ApplicationUserDto>.Success(userDto, StatusCodes.Status201Created);
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine($"Kayıt oluşurken bir hata oldu:{ex.Message}");
+        }
     }
 
     public Task<ResponseDto<NoContent>> ResetPasswordAsync(ResetPasswordDto resetPasswordDto)
