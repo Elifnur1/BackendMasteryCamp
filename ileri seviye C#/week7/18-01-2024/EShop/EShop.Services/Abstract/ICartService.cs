56a76f457d15424cf98bc22b4542ae55d44afa5e
Elifnur1 <elifnurbinici@hotmail.com>
2025-01-25 16:44:58 +0300

    ICARTSERVİCE

--- ileri seviye C#/week7/18-01-2024/EShop/EShop.Services/Abstract/ICartService.cs
+++ ileri seviye C#/week7/18-01-2024/EShop/EShop.Services/Abstract/ICartService.cs
@@ -1,8 +1,13 @@
 using System;
+using Azure;
+using EShop.Shared.Dtos;
+using EShop.Shared.Dtos.ResponseDtos;
 
 namespace EShop.Services.Abstract;
 
 public interface ICartService
 {
+    Task<ResponseDto<CartDto>> GetByIdAsync();
+    Task<ResponseDto<IEnumerable<CartDto>>> GetAllAsync();
 
 }
