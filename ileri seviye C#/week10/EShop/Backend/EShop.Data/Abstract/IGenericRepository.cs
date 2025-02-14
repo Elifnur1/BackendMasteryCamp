c1be733d118e4cacf5a83fed1252fbd87114cfaa
Elifnur1 <elifnurbinici@hotmail.com>
2025-02-14 20:12:38 +0300

    repeat
    proje tekrarı ve kodların amaçlarını tekrarlandı.

--- ileri seviye C#/week10/EShop/Backend/EShop.Data/Abstract/IGenericRepository.cs
+++ ileri seviye C#/week10/EShop/Backend/EShop.Data/Abstract/IGenericRepository.cs
@@ -14,11 +14,11 @@ public interface IGenericRepository<TEntity> where TEntity : class
     Task<IEnumerable<TEntity>> GetAllAsync(
         Expression<Func<TEntity, bool>> predicate = null!,
         Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null!,
-        bool showIsDeleted=false,
-        params Func<IQueryable<TEntity>, IQueryable<TEntity>>[] includes
+        bool showIsDeleted = false,
+        params Func<IQueryable<TEntity>, IQueryable<TEntity>>[] includes //Bu kodun amacı, Include metodu ile ilişkili tabloları çekmek için kullanılır.
     );
-    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> predicate);
-    Task<int> CountAsync();
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> predicate);//Var mı yok mu kontrolü yapar.
+    Task<int> CountAsync();//Kaç kayıt olduğunu sayan metot
     Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate);
     Task<TEntity> AddAsync(TEntity entity);
     void Update(TEntity entity);
