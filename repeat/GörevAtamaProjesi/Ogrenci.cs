8b819dccda69f0eae8710238584f76c4b1e82222
Elifnur1 <elifnurbinici@hotmail.com>
2025-04-17 18:41:06 +0300

    repeat

---
+++ repeat/GörevAtamaProjesi/Ogrenci.cs
@@ -0,0 +1,8 @@
+﻿namespace GörevAtamaProjesi;
+
+public class Ogrenci
+{
+   public string Name { get; set; }
+   public int Age { get; set; }
+   public double Gpa { get; set; }
+}
---
+++ repeat/GörevAtamaProjesi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ogrenci student1 = new Ogrenci();
+        student1.Name = "ELİF";
+        student1.Age = 24;
+        student1.Gpa = 85;
+
+        System.Console.WriteLine($"Ad:{student1.Name}, Yaş:{student1.Age},Not:{student1.Gpa}");
+    }
+
+
+
+}
