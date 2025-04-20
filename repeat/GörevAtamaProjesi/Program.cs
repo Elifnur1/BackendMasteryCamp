e6dacf61a4ede42614d6d43deb1483b886450383
Elifnur1 <elifnurbinici@hotmail.com>
2025-04-20 22:49:57 +0300

    20-04

--- repeat/GörevAtamaProjesi/Program.cs
+++ repeat/GörevAtamaProjesi/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Ogrenci student1 = new Ogrenci();
+        Ogrenci student1 = new Ogrenci(); //Öğrnc, sınıfından bir nesne oluşturduk.
         student1.Name = "ELİF";
         student1.Age = 24;
         student1.Gpa = 85;
 
-        System.Console.WriteLine($"Ad:{student1.Name}, Yaş:{student1.Age},Not:{student1.Gpa}");
+        System.Console.WriteLine($"Ad:{student1.Name}, Yaş:{student1.Age},Not:{student1.Gpa}");  /
     }
 
 
