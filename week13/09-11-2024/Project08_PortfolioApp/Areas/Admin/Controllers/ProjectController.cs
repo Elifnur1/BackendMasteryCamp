f6ba9b65ba02e30681b4a014c8c040aecac9387e
Elifnur1 <elifnurbinici@hotmail.com>
2025-04-29 23:44:13 +0300

    29-04

--- week13/09-11-2024/Project08_PortfolioApp/Areas/Admin/Controllers/ProjectController.cs
+++ week13/09-11-2024/Project08_PortfolioApp/Areas/Admin/Controllers/ProjectController.cs
@@ -12,13 +12,14 @@ namespace Project08_PortfolioApp.Areas.Admin.Controllers
         public async Task<ActionResult> Index()
         {
             //Bağlantıyı hazırlıyoruz
+            //sql server bağlantı yapmamız için gerekli olan connection string.
             var connectionString = "Server=localhost,1441;Database=PortfolioDb;User=sa;Password=YourStrong@Passw0rd;TrustServerCertificate=true";
             var connection = new SqlConnection(connectionString);
 
             //Site ayarlarını çekiyoruz
             var queryProjects = "select * from Projects";
             var projects = await connection.QueryAsync<Project>(queryProjects);
-            
+
             return View(projects);
         }
 
