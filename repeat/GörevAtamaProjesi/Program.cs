fe4d6b3333b0d2a87775f7159293a05437f4f5d4
Elifnur1 <elifnurbinici@hotmail.com>
2025-05-01 21:40:18 +0300

    ---

--- repeat/GörevAtamaProjesi/Program.cs
+++ repeat/GörevAtamaProjesi/Program.cs
@@ -9,7 +9,7 @@ class Program
         student1.Age = 24;
         student1.Gpa = 85;
 
-        System.Console.WriteLine($"Ad:{student1.Name}, Yaş:{student1.Age},Not:{student1.Gpa}");
+        System.Console.WriteLine($"Ad:{student1.Name}, Yaş:{student1.Age},Not:{student1.Gpa}"); //öğrenci nesnesinin özelliklerini ekrana yazdırdık.
     }
 
 
