c14e938b159f623b63f1857cb432e266466ac3ab
Elifnur1 <elifnurbinici@hotmail.com>
2025-05-05 19:05:32 +0300

    05-05

--- repeat/GörevAtamaProjesi/Ogrenci.cs
+++ repeat/GörevAtamaProjesi/Ogrenci.cs
@@ -3,6 +3,6 @@
 public class Ogrenci
 {
    public string Name { get; set; } //Öğrenci sınıfının özelliklerini tutan bir class.
-   public int Age { get; set; }
+   public int Age { get; set; } //öğrenci sınıfıın özelliklerini tutan bir class.
    public double Gpa { get; set; }
 }
