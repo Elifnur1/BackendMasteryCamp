c944cd5d8722422546fdaecbf94a46dcee091473
Elifnur1 <elifnurbinici@hotmail.com>
2025-05-09 22:14:23 +0300

    09-05

--- 25_08_2024/Project07_NumberMethods/Program.cs
+++ 25_08_2024/Project07_NumberMethods/Program.cs
@@ -7,31 +7,31 @@ class Program
 
         #region Aritmetik Operatörler
 
-        int a =10;
-        int b=5;
+        int a = 10;
+        int b = 5;
 
         int result;
-        result=a+b;
-        result= a-b;
-        result =a/b;
-        result=a*b;
-        result=a%b;
-    
-        result= Math.Abs(-10);
-        result=(int)Math.Pow(4,5);
+        result = a + b;
+        result = a - b;
+        result = a / b;
+        result = a * b;
+        result = a % b;
+
+        result = Math.Abs(-10);//mutlak değer.
+        result = (int)Math.Pow(4, 5); //üssü alma
         double result2;
-        result2=Math.Sqrt(16);
-        result2=(int)Math.Round(7.25);
-        result2=Math.Round(7.51);
-        result2=Math.Ceiling(7.25);
-        result2=Math.Floor(7.99);
+        result2 = Math.Sqrt(16);//karakök alma
+        result2 = (int)Math.Round(7.25);//ondalık sayı yuvarlama.
+        result2 = Math.Round(7.51);
+        result2 = Math.Ceiling(7.25);
+        result2 = Math.Floor(7.99);
 
         Console.WriteLine(result2);
-        
-            
+
+
         #endregion
 
-     
-    
+
+
     }
 }
