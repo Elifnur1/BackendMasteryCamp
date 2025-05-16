ced67413db72c0addf2bca5bc31aae1642212a83
Elifnur1 <elifnurbinici@hotmail.com>
2025-05-16 22:33:05 +0300

    ---

--- 14-09-2024/Project20_OOP_Intro/Program.cs
+++ 14-09-2024/Project20_OOP_Intro/Program.cs
@@ -2,31 +2,31 @@
 
 class FirstClass
 {
-     //Buraya sınıfın özellik ve davranışlarını kodluyor olacağız.
-     //Her class bir tiptir.
-     //Özellik: Property
-     //Davranış: Method
+   //Buraya sınıfın özellik ve davranışlarını kodluyor olacağız.
+   //Her class bir tiptir.
+   //Özellik: Property
+   //Davranış: Method
 
-     //Public dışarıy açık
-     //private:buraya özel(belirtilmezse default olarak private)
-     //c# ta ki herşey objedir.
-     public string message;
-     public string age;
-     public void DisplayMessage()
-     {
-        System.Console.WriteLine(message);
-     }
+   //Public dışarıy açık
+   //private:buraya özel(belirtilmezse default olarak private)
+   //c# ta ki herşey objedir.
+   public string message; //string tipinde message değişkeni oluşturduk.
+   public string age;
+   public void DisplayMessage()
+   {
+      System.Console.WriteLine(message);
+   }
 }
 
 class Program
 {
-    static void Main(string[] args)
-    {
-             FirstClass firstClass1=new FirstClass();   
-             firstClass1.message="Selam";
-             //System.Console.WriteLine(firstClass1.message);
-             firstClass1.DisplayMessage();
-             
+   static void Main(string[] args)
+   {
+      FirstClass firstClass1 = new FirstClass();
+      firstClass1.message = "Selam";
+      //System.Console.WriteLine(firstClass1.message);
+      firstClass1.DisplayMessage();
+
 
    }
 }
