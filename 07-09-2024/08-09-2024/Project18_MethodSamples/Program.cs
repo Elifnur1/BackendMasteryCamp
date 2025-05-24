24547bdd19e56d448082ff0ab357c8709fa8a7ad
Elifnur1 <elifnurbinici@hotmail.com>
2025-05-24 21:26:42 +0300

    ---

--- 07-09-2024/08-09-2024/Project18_MethodSamples/Program.cs
+++ 07-09-2024/08-09-2024/Project18_MethodSamples/Program.cs
@@ -4,19 +4,19 @@ class Program
 {
 
     //Kendisine gönderilen int tipindeki sayı listesinde yer alan negatif sayıları geri döndüren bir metot hazırlayalım.
-    static List<int> GetNegativeNumbers(List<int>numbers)
+    static List<int> GetNegativeNumbers(List<int> numbers) //liste yapısında negatif sayıları döndüren metot.
     {
-         List<int> negativeNumbers = new List<int>();
-     foreach (var number in numbers)
-         {
-             if(number<0)
-             {
-                 negativeNumbers.Add(number);
-             }
-         }
-         return negativeNumbers;
+        List<int> negativeNumbers = new List<int>();
+        foreach (var number in numbers)
+        {
+            if (number < 0)
+            {
+                negativeNumbers.Add(number);
+            }
+        }
+        return negativeNumbers;
 
-     }
+    }
 
     static int TotalOfSquaers(int a, int b)
     {
@@ -28,13 +28,13 @@ class Program
 
     static void Main(string[] args)
     {
-         List<int> numbers=[5 ,7 ,-5 ,-4 ,3 ,-12 ,4 ,5, 7];
-         List<int> resultNumbers=GetNegativeNumbers(numbers);
-         foreach (var number in resultNumbers)
-         {
+        List<int> numbers = [5, 7, -5, -4, 3, -12, 4, 5, 7];
+        List<int> resultNumbers = GetNegativeNumbers(numbers);
+        foreach (var number in resultNumbers)
+        {
             Console.WriteLine(number);
         }
-        System.Console.WriteLine(TotalOfSquaers(5,7));
+        System.Console.WriteLine(TotalOfSquaers(5, 7));
 
     }
 
