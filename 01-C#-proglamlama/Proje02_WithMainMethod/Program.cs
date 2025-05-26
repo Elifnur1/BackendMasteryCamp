2caf9f04f05b53586584024b9af0cbae925b84eb
Elifnur1 <elifnurbinici@hotmail.com>
2025-05-26 14:16:00 +0300

    26-05

--- 01-C#-proglamlama/Proje02_WithMainMethod/Program.cs
+++ 01-C#-proglamlama/Proje02_WithMainMethod/Program.cs
@@ -5,16 +5,16 @@ class Program
     static void Main(string[] args)
     {
         System.Console.WriteLine("Bu bizim ilk programımız.");
-    System.Console.WriteLine("Bu ikinci satır.");
-    /*commed (yorum) yapılabilir*/ 
-    //Aşağıdaki görüntüyü oluşturan kodu yazınız. (Her bir satır ayrı bir komutla yazılsın.)
-    //Bugün C#'a giriş yaptık.
-    //Ama öncesinde VS Code ile ilgili ayarlar gerçekleştirdik.
-    //Hazırlayan:Elifnur Binici
-    System.Console.WriteLine("Aşağıdaki görüntüyü oluşturan kodu yazınız. (Her bir satır ayrı bir komutla yazılsın.");
-    System.Console.WriteLine("Bugün C# giriş yaptık.");
-    System.Console.WriteLine("Ama öncesinde VS Code ile ilgili ayarlar gerçekleşrirdik.");
-    System.Console.WriteLine("Hazırlayan");
-    System.Console.WriteLine("Elifnur Binici");
-}
+        System.Console.WriteLine("Bu ikinci satır.");
+        /*commed (yorum) yapılabilir*/
+        //Aşağıdaki görüntüyü oluşturan kodu yazınız. (Her bir satır ayrı bir komutla yazılsın.)
+        //Bugün C#'a giriş yaptık.
+        //Ama öncesinde VS Code ile ilgili ayarlar gerçekleştirdik.
+        //hazırlayan:Elifnur Binici
+        System.Console.WriteLine("Aşağıdaki görüntüyü oluşturan kodu yazınız. (Her bir satır ayrı bir komutla yazılsın.");
+        System.Console.WriteLine("Bugün C# giriş yaptık.");
+        System.Console.WriteLine("Ama öncesinde VS Code ile ilgili ayarlar gerçekleşrirdik.");
+        System.Console.WriteLine("Hazırlayan");
+        System.Console.WriteLine("Elifnur Binici");
+    }
 }
