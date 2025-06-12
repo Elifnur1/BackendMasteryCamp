742541b4e9fef8fc6dc98ecd0d4ccb8e60c534dc
Elifnur1 <elifnurbinici@hotmail.com>
2025-06-12 17:27:46 +0300

    12-06

--- 14-09-2024/15-09-2024/Project24_Interface/Program.cs
+++ 14-09-2024/15-09-2024/Project24_Interface/Program.cs
@@ -1,11 +1,11 @@
 ﻿using Project24_Interface.Interfaces;
 
-namespace Project24_Interface;
+namespace Project24_Interface; //Interface(arayüz) tanımlama--üst derece sınıf.
 
 class Program
 {
     static void Main(string[] args)
     {
-        IRepo repo=new ProductRepo();
+        IRepo repo = new ProductRepo();
     }
 }
